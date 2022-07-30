2b74c3e253283b5ee2291b67c44840e1f18b7c9e
Elvin Musayev <e.musayev590@gmail.com>
2022-07-31 01:34:53 +0400

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -20,3 +20,26 @@ const swiper = new Swiper('.swiper-container', {
   },
   // effect: "coverflow",
 });
+
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  // effect: "coverflow",
+});
